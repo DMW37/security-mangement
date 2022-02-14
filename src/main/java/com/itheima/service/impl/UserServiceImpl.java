651bf26b40a652d7b1e5237fffab50bee12d25c6
deng Mingwei <3561294402@qq.com>
2022-02-14 18:40:38 +0800

    SpringSecurity 通过数据库认证

--- src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -6,12 +6,15 @@ import com.itheima.domain.SysUser;
 import com.itheima.service.RoleService;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @Transactional
@@ -50,4 +53,30 @@ public class UserServiceImpl implements UserService {
         }
     }
 
+    /**
+     * @param username 用户输入的用户名
+     * @return UserDetails springSecurity自己的用户对象
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            SysUser sysUser = userDao.findByName(username);
+            if (sysUser == null) {
+                return null;
+            }
+            List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            List<SysRole> roles = sysUser.getRoles();
+            Iterator<SysRole> iterator = roles.iterator();
+            while (iterator.hasNext()) {
+                authorities.add(new SimpleGrantedAuthority(iterator.next().getRoleName()));
+            }
+            UserDetails userDetails = new User(sysUser.getUsername(), "{noop}" + sysUser.getPassword(), authorities);
+            return userDetails;
+        } catch (Exception e) {
+            e.printStackTrace();
+            //认证失败
+            return null;
+        }
+    }
 }
