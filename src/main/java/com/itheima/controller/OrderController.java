100729b31a4f088874c367078f70171016ebfd30
deng Mingwei <3561294402@qq.com>
2022-02-15 15:11:20 +0800

    @RolesAllowed,@PreAuthorize 两注解的使用

--- src/main/java/com/itheima/controller/OrderController.java
+++ src/main/java/com/itheima/controller/OrderController.java
@@ -1,13 +1,18 @@
 package com.itheima.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.security.RolesAllowed;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
-    @Secured("{ROLE_ORDER,ROLE_ADMIN}")
+    //@Secured("{ROLE_ORDER,ROLE_ADMIN}")  //springSecurity框架内部提供
+    //@RolesAllowed("{ROLE_ORDER,ROLE_ADMIN}")  //jsr250提供
+    @PreAuthorize("hasAnyRole('ROLE_ORDER','ROLE_ADMIN')") //spring提供 需要开启spring的el写法
     @RequestMapping("/findAll")
     public String findAll(){
         return "order-list";
